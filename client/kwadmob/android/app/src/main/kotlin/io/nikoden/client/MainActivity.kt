130b7619926996e6ac5abb8b13386c00edf9d69a
Nicolas Denoël <github@nikoden.io>
2023-12-01 12:02:25 +0100

    Fresh empty Flutter project

---
+++ client/kwadmob/android/app/src/main/kotlin/io/nikoden/client/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.nikoden.client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- client/kwadramob/android/app/src/main/kotlin/io/nikoden/kwadra/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.nikoden.kwadra
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
